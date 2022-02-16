5225799d9741fe5644b2fe47501f4926879bd688
Hong Phuc <hongphuc.dthp@gmail.com>
2022-02-16 15:06:13 +0700

    handle send confirmation/confirmed email

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { MailerService } from '@nestjs-modules/mailer';
+
+import { User } from './app.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private code;
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private mailerService: MailerService,
+  ) {
+    this.code = Math.floor(10000 + Math.random() * 90000);
+  }
+
+  async sendConfirmationEmail(user: any) {
+    const { email, fullname } = user;
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Please confirm your email',
+      template: 'confirm',
+      context: { fullname, code: this.code },
+    });
+  }
+
+  async sendConfirmedEmail(user: User) {
+    const { email, fullname } = user;
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Your email has been confirmed',
+      template: 'confirmed',
+      context: { email, fullname },
+    });
   }
 }

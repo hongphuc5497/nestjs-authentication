6bfde199f2a80db50ad7e3e803fca3c9d9243755
Hong Phuc <hongphuc.dthp@gmail.com>
2022-02-16 15:49:10 +0700

    config app module, add dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
+import crypto from 'crypto';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './app.entity';
+
+const JWT_SECRET =
+  process.env.JWT_SECRET || crypto.randomBytes(64).toString('hex');
 
 @Module({
   imports: [
@@ -17,6 +27,31 @@ import { User } from './app.entity';
       logging: true,
     }),
     TypeOrmModule.forFeature([User]),
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        secure: false,
+        auth: {
+          user: 'your email address',
+          password: 'your email password',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <your_email@gmail.com>',
+      },
+      template: {
+        dir: join(__dirname, '../views/email-templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
